cdfc51e25748c2623374c1e3c7d632bf8c0c3f40
emon3455 <emon.mhk69@gmail.com>
2023-02-06 23:19:50 +0600

    initial commit

---
+++ 1problem.js
@@ -0,0 +1,20 @@
+function mindGame(number){
+
+    /* Atfirst it will multiply the number with 3 then it will add 10 with the number after that it will multiply the number with 2 then lastly it will substract 5 from the number and finally return the number 
+    */
+   if(typeof number != "number" || number  < 0){
+    console.log("please enter a valid input");
+    return;
+   }
+   
+   number *= 3;
+   number += 10;
+   number /= 2;
+   number -=  5;
+
+   return number;
+
+}
+
+let res = mindGame(5);
+console.log(res);
