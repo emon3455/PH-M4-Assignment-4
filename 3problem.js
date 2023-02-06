3ed8b33ce4153e99e23deede98bd833d530ffa79
emon3455 <emon.mhk69@gmail.com>
2023-02-07 00:03:15 +0600

    problem 3 done

---
+++ 3problem.js
@@ -0,0 +1,24 @@
+function isLGSeven(number){
+
+    /*  
+    here we have to find the difference between the given number and 7.
+    if the difference is less then 7 return the difference otherwise return the double of the given number
+     */
+
+    if(typeof number != "number"){
+        return "please enter a valid input";
+    }
+
+    let difference = number - 7;
+
+    if(difference < 7){
+        return difference;
+    }
+    else{
+        return number*2;
+    }
+
+}
+
+let res = isLGSeven(15);
+console.log(res);
--- problem.js
+++ problem.js
@@ -1,6 +1,6 @@
 function mindGame(number){
 
-    /* Atfirst it will multiply the number with 3 then it will add 10 with the number after that it will multiply the number with 2 then lastly it will substract 5 from the number and finally return the number 
+    /* Atfirst it will multiply the given number with 3 then it will add 10 with the number after that it will multiply the number with 2 then lastly it will substract 5 from the number and finally return the number 
     */
     if(typeof number != "number" || number  < 0){
         return "please enter a valid input";
@@ -19,8 +19,8 @@ function mindGame(number){
 
 function evenOdd(message){
 
-    // Here in this function we are checking is the length of the message is even or odd.
-    
+    // Here in this function we are checking is the length of the given message is even or odd.
+
     if(typeof message != "string"){
         return "please enter a valid input";
     }
@@ -32,6 +32,29 @@ function evenOdd(message){
     }
     else{
         return "odd";
+    }
+
+}
+
+
+function isLGSeven(number){
+
+    /*  
+    here we have to find the difference between the given number and 7.
+    if the difference is less then 7 return the difference otherwise return the double of the given number
+     */
+
+    if(typeof number != "number"){
+        return "please enter a valid input";
+    }
+
+    let difference = number - 7;
+
+    if(difference < 7){
+        return difference;
+    }
+    else{
+        return number*2;
     }
 
 }
